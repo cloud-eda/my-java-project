c51ee1f88f829f7862fd0587d59d87721a675738
Oleksandr Andriienko <oandriie@redhat.com>
2023-11-28 01:05:06 +0200

    Test Jenkins

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,7 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void m ain( String[] args )
     {
         System.out.println( "Hello Jenkins!" );
     }
