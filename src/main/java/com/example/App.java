dd2379c97dc475572e99d6afd30e75b62768ecc5
Oleksandr Andriienko <oandriie@redhat.com>
2023-11-28 02:41:27 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,7 @@ package com.example;
  */
 public class App 
 {
-    public static void m ain( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "Hello Jenkins!" );
     }
